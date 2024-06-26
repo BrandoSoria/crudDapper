4de1bdcc5533ff1994db3015e20634ad27a67ff4
brandon <webarebrandy@gmail.com>
2024-06-25 20:06:23 -0600

    crear clase para employee

---
+++ Tables/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crudDapper.Tables
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
