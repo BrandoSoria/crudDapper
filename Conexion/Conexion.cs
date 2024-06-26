c6b2777800e4d4d10f9757fd8e50f0e793c8b7dd
brandon <webarebrandy@gmail.com>
2024-06-25 20:52:31 -0600

    abriendo la conexion y haciendo una consulta

--- Conexion/Conexion.cs
+++ Conexion/Conexion.cs
@@ -6,18 +6,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Dapper;
+using crudDapper.Tables;
 
 namespace crudDapper.Conexion
 {
-    public static class Conexion
+    public class Conexion
     {
-        public static IDbConnection Connection
+        private IDbConnection Connection
 
         {
             get
             {
                 return new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
             }
+
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Employee>("SELECT * FROM employee");
+            }
+
 
         }
     }
